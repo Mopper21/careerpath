ca1a6a1389f0ccc35229e1c15bff457e862544ef
Mopper21 <152798669+Mopper21@users.noreply.github.com>
2024-05-21 09:24:32 +0200

    commit

---
+++ careerpath/src/config/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVhdwoMIKehFTatNdln5MjAI15-m412es",
+  authDomain: "careerpath-d51e8.firebaseapp.com",
+  databaseURL: "https://careerpath-d51e8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "careerpath-d51e8",
+  storageBucket: "careerpath-d51e8.appspot.com",
+  messagingSenderId: "566451300543",
+  appId: "1:566451300543:web:0fd05f65f4863216947e0a",
+  measurementId: "G-PCQQ1DNK8H"
+};
+ 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
