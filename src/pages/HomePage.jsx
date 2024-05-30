8f0fa914ba611ed4238389ed8d98cdd321c28525
Mopper21 <152798669+Mopper21@users.noreply.github.com>
2024-05-30 20:36:07 +0200

    home page i app.css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ function HomeSection() {
   return (
     <section id="hero" className="d-flex align-items-center">
       <div className="container d-flex flex-column align-items-center" data-aos="zoom-in" data-aos-delay="100">
-        <h2>Connect. Learn. Succeed.</h2>
+        <h1 id="h1-des">Connect <br />Learn <br />Succed</h1>
         <div id="auth-container" className="text-center">
          
         </div>
